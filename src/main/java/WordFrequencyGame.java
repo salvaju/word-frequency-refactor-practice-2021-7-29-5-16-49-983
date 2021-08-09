2d5a42062f52995949689bded3dd2e083d08ae91
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-09 15:19:44 +0800

    [WordFrequencyGame][Juan Carlos Salvador] refactor: change name of inputStr into sentence

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr) {
+    public String getResult(String sentence) {
 
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
+        if (sentence.split("\\s+").length == 1) {
+            return sentence + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = sentence.split("\\s+");
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
