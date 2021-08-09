b9262b632ec9b16428380cacfc3b70237f2d9161
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-09 19:47:41 +0800

    Removed unused import

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -1,7 +1,6 @@
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class WordFrequencyGameTest {
 
